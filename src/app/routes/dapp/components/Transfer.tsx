1129ef7136ce8f5c0c14bce60d60737781ee1ad3
fivge <luanxingtong@gmail.com>
2023-06-25 18:02:55 +0800

    feat(dapp): feat: b9

--- src/app/routes/dapp/components/Transfer.tsx
+++ src/app/routes/dapp/components/Transfer.tsx
@@ -5,12 +5,12 @@ export function Transfer({ transferTokens, tokenSymbol }) {
     <div>
       <h4>Transfer</h4>
       <form
-        onSubmit={(event) => {
+        onSubmit={event => {
           // This function just calls the transferTokens callback with the
           // form's data.
           event.preventDefault();
 
-          const formData = new FormData(event.target);
+          const formData = new FormData(event.target as any);
           const to = formData.get("to");
           const amount = formData.get("amount");
 
@@ -21,14 +21,7 @@ export function Transfer({ transferTokens, tokenSymbol }) {
       >
         <div className="form-group">
           <label>Amount of {tokenSymbol}</label>
-          <input
-            className="form-control"
-            type="number"
-            step="1"
-            name="amount"
-            placeholder="1"
-            required
-          />
+          <input className="form-control" type="number" step="1" name="amount" placeholder="1" required />
         </div>
         <div className="form-group">
           <label>Recipient address</label>
